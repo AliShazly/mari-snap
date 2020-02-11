cac0cce92e4a482cf67ace8244403f45afcc15d1
Ali <alishazly302@gmail.com>
2020-02-11 13:57:08 -0800

    Finished proof of concept

--- snap.py
+++ snap.py
@@ -1,7 +1,7 @@
 import mari
 import xml.etree.ElementTree as ET
 
-# https://community.foundry.com/discuss/topic/143466/a-trick-to-set-your-node-position-via-python
+
 def getNodePosition(graph, node):
     nodes = [node]
 
@@ -12,43 +12,40 @@ def getNodePosition(graph, node):
     root = ET.fromstring(nodestring)
 
     # Node Position as List
-    node_x_y = []
+    nodePos = []
 
     # Find the first Node Position entry in the XML
     # Always the first because on Group Nodes multiple nodes are inside.
     for position in root.iter('position'):
         try:
-            nodeposition = position.text
-            lst = nodeposition.split(',')
-            for nr in lst:
-                node_x_y.append(float(nr))
+            lst = position.text.split(',')
+            for i in lst:
+                nodePos.append(float(i))
             break
         except:
             pass
-    return node_x_y
+    return nodePos
 
 
 def offsetNodePosition(graph, node, offsetX, offsetY):
-    nodestring = graph.nodesToString(nodes)
-    node_x_y,node_pos_str = getNodePosition(graph,node)
-    if len(node_x_y) != 0:
-        # Set a new Node Position with offset
-        newValueX = node_x_y[0] + offsetX
-        newValueY =  node_x_y[1] + offsetY
-        node_x_y[0] = newValueX
-        node_x_y[1] = newValueY
-        new_node_pos_str = str(node_x_y[0]) + ',' + str(node_x_y[1])
 
-        # Replace nodeposition text in nodestring with new one
-        toReplace = '<position Type="QPointF">' + node_pos_str + '</position>'
-        replaceWith = '<position Type="QPointF">' + new_node_pos_str + '</position>'
-        nodestring = nodestring.replace(toReplace,replaceWith)
+    nodestring = graph.nodesToString([node])
+    nodePos = getNodePosition(graph, node)
+    stringNodePos = "{},{}".format(nodePos[0], nodePos[1])
 
-    # Generate Node at new position
-    newNode = graph.nodesFromString(nodestring)
+    # Set a new Node Position with offset
+    newX = nodePos[0] + offsetX
+    newY = nodePos[1] + offsetY
+    newStringNodePos = "{},{}".format(newX, newY)
 
-    for n in nodes:
-        graph.removeNode(n)
+    # Replace nodeposition text in nodestring with new one
+    toReplace = "<position Type=\"QPointF\">{}</position>".format(stringNodePos)
+    replaceWith = "<position Type=\"QPointF\">{}</position>".format(newStringNodePos)
+    nodestring = nodestring.replace(toReplace,replaceWith)
+
+    # Generate Node at new position and remove old one
+    newNode = graph.nodesFromString(nodestring)[0]
+    graph.removeNode(node)
 
     return newNode
 
@@ -71,12 +68,14 @@ def setNodeIO(node, inputs, outputs):
     for (inputPort, inputNode), (outputPort, outputNode) in zip(inputs.items(), outputs.items()):
         node.setInputNode(inputPort, inputNode)
         outputNode.setInputNode(outputPort, node)
-        
-        
+
+
+def moveNode(graph, node, offsetX, offsetY):
+    inputs, outputs = getNodeIO(node)
+    newNode = offsetNodePosition(graph, node, offsetX, offsetY)
+    setNodeIO(newNode, inputs, outputs)
+    return newNode
 
 nodeGraph = mari.geo.current().nodeGraph()
 selectedNodes = nodeGraph.selectedNodeList()
-
-inputs, outputs = getNodeIO(n)
-setNodeIO(n2, inputs, outputs)
-
+moveNode(nodeGraph, selectedNodes[0], 50, 50)
